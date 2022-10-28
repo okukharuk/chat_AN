8a0e87bc9b1ddd6f027b5cc6b4ce43dc8ee47d4f
VipDipp <kuxaruk03@ukr.net>
2022-10-28 23:27:27 +0300

    feat: server and client initialize

---
+++ client/src/components/Chat.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-[75%] w-1/3 border-2 rounded border-black">
+      <div className="mb-auto">Anon Chat</div>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Chat from '../components/Chat';
+
+const HomePage = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <Chat />
+    </div>
+  );
+};
+
+export default HomePage;
